a2b03cc584289fcfafc1ebb1c5136c29bcd56234
Uwiagod <sensigod466@gmail.com>
2026-01-12 20:33:24 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-particlesJS("particles-js", {
-  particles: {
-    number: { value: 120 },
-    color: { value: ["#ff0000", "#ffffff"] },
-    shape: { type: "circle" },
-    opacity: { value: 0.6 },
-    size: { value: 2 },
-    move: {
-      enable: true,
-      speed: 0.6
-    }
-  },
-  interactivity: {
-    events: {
-      onhover: { enable: true, mode: "repulse" }
-    }
-  },
-  retina_detect: true
-});
+console.log("Glory Cheat Hub Loaded - Welcome Developer");
+
+// We can add a mobile menu toggle here later if you want!
