554af76f1897f24427f7c4cb3f0c52f5ac618f25
Uwiagod <sensigod466@gmail.com>
2026-01-12 11:12:24 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 particlesJS("particles-js", {
   particles: {
     number: { value: 120 },
-    color: { value: "#00ff9c" },
+    color: { value: ["#ff0000", "#ffffff"] },
     shape: { type: "circle" },
-    opacity: { value: 0.4 },
+    opacity: { value: 0.6 },
     size: { value: 2 },
     move: {
       enable: true,
-      speed: 1,
-      direction: "none"
+      speed: 0.6
     }
   },
   interactivity: {
